ed9de76ad4246627bc36d1e83276e9fe1b9eceb3
Михаил <gmike103g@gmail.com>
2024-01-22 05:56:26 +0500

    created page components and configured roaming

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LayoutComponent } from "./theme/layout/layout.component";
+import { CompanyListComponent}  from "./pages/company-list/company-list.component";
+import { CompanyDetailComponent } from "./pages/company-detail/company-detail.component";
+import { CompanyYandexMapComponent } from './pages/company-yandex-map/company-yandex-map.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full'
+      },
+      {
+        path: 'list',
+        component: CompanyListComponent
+      },
+      {
+        path: 'detail/:id',
+        component: CompanyDetailComponent
+      },
+      {
+        path: 'map',
+        component: CompanyYandexMapComponent
+      }
+    ]
+  }
+];
---
+++ src/app/pages/company-yandex-map/company-yandex-map.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-yandex-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-yandex-map.component.html',
+  styleUrl: './company-yandex-map.component.scss'
+})
+export class CompanyYandexMapComponent {
+
+}
